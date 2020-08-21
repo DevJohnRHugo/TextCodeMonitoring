5219d1948a414f38bfe7c6aa082dffe92dd14df4
John Hugo <johnayossulit@gmail.com>
2020-08-21 11:59:48 +0800

    rebase/squash

--- TextCodeMonitoring/Program.cs
+++ TextCodeMonitoring/Program.cs
@@ -17,7 +17,8 @@ namespace TextCodeMonitoring
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TextCodeMonitoringForm());
-
+            //skdjalksjl
+            //commit 2
         }
     }
 }
